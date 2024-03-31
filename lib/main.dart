199bd83e73ef8b09bbcf8a8dd4b229377e144336
HayataTakagi <h.takagi.work@gmail.com>
2024-03-31 17:04:29 +0900

    水平を線で描画

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:aeyrium_sensor/aeyrium_sensor.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
@@ -36,6 +38,8 @@ class _MyHomePageState extends State<MyHomePage> {
   double g_x = 0;
   double g_y = 0;
   double g_z = 0;
+  double pitch = 0;
+  double roll = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -48,9 +52,19 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(_userAccelerometerValues),
             Text(_gyroscopeValues),
-            Text("x: ${g_x.toStringAsFixed(2)}"),
-            Text("y: ${g_y.toStringAsFixed(2)}"),
-            Text("z: ${g_z.toStringAsFixed(2)}"),
+            Text(
+                "ジャイロ累積値\nx: ${g_x.toStringAsFixed(2)}\ny: ${g_y.toStringAsFixed(2)}\nz: ${g_z.toStringAsFixed(2)}"),
+            Text(
+                "pitch: ${pitch.toStringAsFixed(2)}\nroll: ${roll.toStringAsFixed(2)}"),
+            Container(
+              transform: Matrix4.rotationZ(roll),
+              transformAlignment: Alignment.center,
+              child: Container(
+                width: 2000,
+                  decoration: const BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.green)),
+              )),
+            )
           ],
         ));
   }
@@ -62,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
       (UserAccelerometerEvent event) {
         setState(() {
           _userAccelerometerValues =
-              "加速度センサー\n${event.x}\n${event.y}\n${event.z}";
+              "加速度センサー\nx:${event.x}\ny:${event.y}\nz:${event.z}";
         });
       },
     );
@@ -72,9 +86,18 @@ class _MyHomePageState extends State<MyHomePage> {
           g_x += event.x;
           g_y += event.y;
           g_z += event.z;
-          _gyroscopeValues = "ジャイロセンサー\n${event.x}\n${event.y}\n${event.z}";
+          _gyroscopeValues =
+              "ジャイロセンサー\nx:${event.x}\ny:${event.y}\nz:${event.z}";
         });
       },
     );
+    AeyriumSensor.sensorEvents?.listen((SensorEvent event) {
+      //do something with the event , values expressed in radians
+      // print("Pitch ${event.pitch} and Roll ${event.roll}");
+      setState(() {
+        pitch = event.pitch;
+        roll = event.roll;
+      });
+    });
   }
 }
